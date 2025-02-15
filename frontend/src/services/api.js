44b8ed21343abbf85d638e1054dd03b6578ecbf6
Reid Pace <rwpace06@gmail.com>
2025-02-14 21:12:39 -0800

    Final

---
+++ frontend/src/services/api.js
@@ -0,0 +1,15 @@
+const API_KEY = "41bd6984698a28257932e2655b608dd2";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json()
+    return data.results
+};
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)
+    }`);
+    const data = await response.json()
+    return data.results
+};
